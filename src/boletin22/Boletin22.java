d581871067fc7e632da7f1ae98d54afd58ef4182
ablancoabalde <ablancoabalde@victoria6.danielcastelao.org>
2018-04-18 11:46:52 +0200

    Finalizado

---
+++ src/boletin22/Boletin22.java
@@ -0,0 +1,17 @@
+
+package boletin22;
+
+/**
+ *
+ * @author ablancoabalde
+ */
+public class Boletin22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
